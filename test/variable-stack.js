9cd4f362619959724c5b110705b073b69d18c324
Sebastian Langer <langer@bestit-online.de>
2016-07-22 20:16:09 +0200

    test peekLayer completely, fixes #14

--- test/variable-stack.js
+++ test/variable-stack.js
@@ -146,6 +146,30 @@ it('allows layer-peeking & allPop', function(){
     expect(this.stack.value.child.child.child.child.fixture).to.equal('fixture');
 });
 
+it('properly supports layer-peeking', function(){
+    this.stack.push('child1');
+    this.stack.value.val = 'val-1';
+    this.stack.pop();
+
+    this.stack.push('child2');
+    this.stack.value.val = 'val-2';
+    this.stack.pop();
+
+    this.stack.push('child3');
+    this.stack.value.val = 'val-3';
+    expect(this.stack.peekLayer().value.child1.val).to.equal('val-1');
+    expect(this.stack.peekLayer().value.child2.val).to.equal('val-2');
+
+    this.stack.push('child31');
+    this.stack.value.val = 'val-3-1';
+    expect(this.stack.peekLayer().value.val).to.equal('val-3');
+    expect(this.stack.peekLayer(2).value.child1.val).to.equal('val-1');
+    expect(this.stack.peekLayer(2).value.child2.val).to.equal('val-2');
+
+    this.stack.push('child311');
+    expect(this.stack.peekLayer(3).value.child1.val).to.equal('val-1');
+});
+
 it('prevents invalid popping', function(){
     expect(this.stack.pop.bind(this.stack)).to.throw(Error);
 
