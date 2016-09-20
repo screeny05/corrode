9ca18ba2fca181396be3fd593dfb9677b1d65189
Sebastian Langer <langer@bestit-online.de>
2016-09-20 22:32:33 +0200

    add jsdocs to utils - fixes #4

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,25 @@
 import { mapValues } from 'lodash';
 
+/**
+ * bind each function in an object with a tap to a given context
+ * @param  {object} obj object with functions
+ * @param  {object} ctx context
+ * @return {object}     copy of object with each function wrapped in a tap
+ */
 exports.tapBindObject = function(obj, ctx){
     return mapValues(obj, function(fn){
-        return function(...args){
+        return typeof fn === 'function' ? function(...args){
             return ctx.tap(fn.bind(ctx, ...args));
-        };
+        } : fn;
     });
 };
 
+/**
+ * bind each function in an object to a given context
+ * @param  {object} obj object with functions
+ * @param  {object} ctx context
+ * @return {object}     copy of object obj with each function bound to ctx
+ */
 exports.bindObject = function(obj, ctx){
-    return mapValues(obj, fn => fn.bind(ctx));
+    return mapValues(obj, fn => typeof fn === 'function' ? fn.bind(ctx) : fn);
 };
